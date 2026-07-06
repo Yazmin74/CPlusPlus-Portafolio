f2ebd7a54c4490b4ebf8490e9aaa0d2f56b5e8bb
AngelaMogollon-Dev <yazmin.mogollon@hotmail.com>
2026-07-05 19:25:36 -0700

    Add AverageAge.cpp to calculate average age

---
+++ 01-Basics/AverageAge.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double age1 = 19;
+
+    double age2 = 21;
+
+    double age3 = 30;
+
+    double averageAge;
+
+    averageAge = (age1 + age2 + age3) / 3;
+
+    cout << "The average age is " << averageAge;
+
+}
