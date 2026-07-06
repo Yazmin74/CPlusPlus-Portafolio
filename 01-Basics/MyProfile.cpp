d16b6158e5f5e209885150208c7abbc3d1b66c81
AngelaMogollon-Dev <yazmin.mogollon@hotmail.com>
2026-07-05 19:27:47 -0700

    Add MyProfile.cpp with information display

---
+++ 01-Basics/MyProfile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int age;
+    double birthWeight;
+    string firstName;
+    char gender;
+    
+    firstName = "Angela Yazmin Mogollon";
+    birthWeight = 2.613;
+    age = 30;
+    gender = 'F';
+
+    cout << "My name is " << firstName << endl;
+    cout << "..." << endl;
+    cout << "My age is " << age  << " years old." << endl;
+    cout << "..." << endl;
+    cout << "When I was born my weight was " << birthWeight << " pounds." << endl;
+    cout << "..." << endl;
+    cout << "My Gender is " << gender << endl;
+    return 0;
+}
+
