2af7409f7a7e038233390672730e0c8e177f92f4
AngelaMogollon-Dev <yazmin.mogollon@hotmail.com>
2026-07-05 20:19:20 -0700

    Add temperature forecast display for Pleasant Hill
    This program displays the high temperatures for a week in Pleasant Hill, California, along with a source.

---
+++ 01-Basics/MyCity2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ // define the Temperatures
+ int temp [5] = {74, 85, 75, 73, 81};
+
+ // Display The Header
+cout << "Pleasant Hill, California, Forecast High Temperatures: " << endl;
+
+ // Display the temperatures 
+cout << "Monday, Sep 25, " << temp[0] << " degrees" << endl;
+cout << "Tuesday, Sep 26, " << temp[1] << " degrees" << endl;
+cout << "Wednesday, Sep 27, " << temp[2] << " degrees" << endl;
+cout << "Thursday, Sep 28, " << temp[3] << " degrees" << endl;
+cout << "Friday, Sep 29, " << temp[4] << " degrees" << endl; 
+
+ // Display the source
+cout << "source: www.todaysweather.com " << endl;
+}
