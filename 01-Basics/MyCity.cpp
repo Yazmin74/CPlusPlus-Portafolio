82935769a15e32b105f821a2cb0413998261fd43
AngelaMogollon-Dev <yazmin.mogollon@hotmail.com>
2026-07-05 19:28:43 -0700

    Add weather forecast program for Pleasant Hill

---
+++ 01-Basics/MyCity.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int temp1;
+int temp2;
+int temp3;
+int temp4;
+int temp5;
+
+temp1 = 74;
+temp2 = 85;
+temp3 = 75;
+temp4 = 73; 
+temp5 = 81; 
+
+
+cout << "Pleasant Hill, California, Forecast High Temperatures: " << endl;
+cout << "Monday, Sep 25, " << temp1 << " degrees" << endl;
+cout << "Tuesday, Sep 26, " << temp2 << " degrees" << endl;
+cout << "Wednesday, Sep 27, " << temp3 << " degrees" << endl;
+cout << "Thursday, Sep 28, " << temp4 << " degrees" << endl;
+cout << "Friday, Sep 29, " << temp5 << " degrees" << endl; 
+cout << "source: www.todaysweather.com " << endl;
+cout << endl;
+}
